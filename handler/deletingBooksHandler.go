ad20341bb50725829676722a22ca45def8f53365
Arley <enginegarley@gmail.com>
2025-03-31 19:35:13 -0300

    feat: add delete book

--- handler/deletingBooksHandler.go
+++ handler/deletingBooksHandler.go
@@ -1,13 +1,30 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/ArleyAlbuquerque/book-tracker/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func DeletingBooksHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Book deleted",
-	})
+	id := c.Query("id")
+	if id == " " {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	creating := schemas.Creating{}
+
+	if err := db.First(&creating, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, fmt.Sprintf("book with id: %s not found", id))
+		return
+	}
+
+	if err := db.Delete(&creating).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, fmt.Sprintf("error deleting book with id: %s", id))
+		return
+	}
+	sendSucess(c, "book-deleted", creating)
 }
