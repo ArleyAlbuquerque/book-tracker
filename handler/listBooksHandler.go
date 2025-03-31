38cfd8a98d088519e05f30a1a6bce2b3b6889e49
Arley <enginegarley@gmail.com>
2025-03-31 19:35:49 -0300

    feat: add show a book

--- handler/listBooksHandler.go
+++ handler/listBooksHandler.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/ArleyAlbuquerque/book-tracker/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListBooksHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Show all books",
-	})
+	books := []schemas.Creating{}
+
+	if err := db.Find(&books).Error; err != nil {
+		sendError(c, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSucess(c, "list-books", books)
+
 }
