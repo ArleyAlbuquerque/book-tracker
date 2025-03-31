76ea4ee0824464fd974af49c959299fa1c436899
Arley <enginegarley@gmail.com>
2025-03-31 19:35:34 -0300

    feat: add show all books

--- handler/showingBooksHandler.go
+++ handler/showingBooksHandler.go
@@ -3,11 +3,22 @@ package handler
 import (
 	"net/http"
 
+	"github.com/ArleyAlbuquerque/book-tracker/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowingBooksHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "BOOK",
-	})
+	id := c.Query("id")
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	creating := schemas.Creating{}
+	if err := db.First(&creating, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, "book not found")
+		return
+	}
+
+	sendSucess(c, "show-book", creating)
 }
