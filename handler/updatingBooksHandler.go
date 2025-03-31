61f0b079c1d2427fe47ffdce7b28aeedd389b3e7
Arley <enginegarley@gmail.com>
2025-03-31 19:36:02 -0300

    feat: add update book handler

--- handler/updatingBooksHandler.go
+++ handler/updatingBooksHandler.go
@@ -3,11 +3,50 @@ package handler
 import (
 	"net/http"
 
+	"github.com/ArleyAlbuquerque/book-tracker/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdatingBooksHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Book updated",
-	})
+	request := UpdateBookRequest{}
+
+	c.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.ErrF("validation error:%v", err.Error())
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := c.Query("id")
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+	creating := schemas.Creating{}
+
+	if err := db.First(&creating, id).Error; err != nil {
+		sendError(c, http.StatusNotFound, "book not found")
+		return
+	}
+	//Update book
+	if request.Name != "" {
+		creating.Name = request.Name
+	}
+	if request.Autor != "" {
+		creating.Autor = request.Autor
+	}
+	if request.Pages > 0 {
+		creating.Pages = request.Pages
+	}
+
+	//save
+	if err := db.Save(&creating).Error; err != nil {
+		logger.ErrF("error updating book: %v", err.Error())
+		sendError(c, http.StatusInternalServerError, "error updating book")
+		return
+	}
+
+	sendSucess(c, "uptade-book", creating)
+
 }
